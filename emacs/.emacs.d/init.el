a980c02e18276363f3d6718c39ead3542452a6fd
Antonio Aslan@ <aslan.suarez@gmail.com>
2022-02-01 21:06:05 +0100

    Fixing ivy and counsel, buffer too

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -172,11 +172,11 @@
 
 ;; Company
 (require 'company)
-(with-eval-after-load 'company
-  (define-key company-active-map (kbd "M-n") nil)
-  (define-key company-active-map (kbd "M-p") nil)
-  (define-key company-active-map (kbd "C-n") #'company-select-next)
-  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+(with-eval-after-load 'company)
+  ;; (define-key company-active-map (kbd "M-n") nil)
+  ;; (define-key company-active-map (kbd "M-p") nil)
+  ;; (define-key company-active-map (kbd "C-n") #'company-select-next)
+  ;; (define-key company-active-map (kbd "C-p") #'company-select-previous))
 (add-hook 'after-init-hook 'global-company-mode)
 (define-key global-map (kbd "C-.") 'company-files)
 
@@ -213,10 +213,21 @@
 (ivy-mode 1)
 (all-the-icons-ivy-rich-mode 1)
 
-; set ivy hotkeys
-(global-set-key (kbd "C-s") 'swiper)
-(global-set-key (kbd "M-x") 'counsel-M-x) ; counsel search command
-(global-set-key (kbd "C-x C-f") 'counsel-find-file) 
+; Ivy-based interface to standard commands
+(global-set-key (kbd "C-s") 'swiper-isearch)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "M-y") 'counsel-yank-pop)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+(global-set-key (kbd "C-c n") 'counsel-fzf)
+(global-set-key (kbd "C-c b") 'counsel-bookmark)
+(global-set-key (kbd "C-c o") 'counsel-outline)
+(global-set-key (kbd "C-c t") 'counsel-load-theme)
+(global-set-key (kbd "C-x w") 'kill-buffer-and-window)
+
 ;; projectile
 (projectile-mode 1)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
@@ -385,11 +396,6 @@
   "'" '(term :which-key "iterm")
   ;; magit
   "m" '(magit :which-key "magit")
-  ;; buffers
-  "bn" '(evil-next-buffer :which-key "next buffer")
-  "bp" '(evil-previous-buffer :which-key "previous buffer")
-  "bd" '(evil-delete-buffer :which-key "delete buffer")
-  "bb" '(helm-buffers-list :which-key "change buffer")
   ;; Visual toggles
   "vl" '(display-line-numbers-mode :which-key "toggle line numbers")
   "vt" '(toggle-truncate-lines :which-key "toggle truncated lines")
