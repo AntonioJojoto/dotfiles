09c66431a59fbce1a2a2d110fc86451b0f825c65
Antonio Aslan@ <aslan.suarez@gmail.com>
2022-05-11 23:03:24 +0200

    Different Files

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,20 +1,5 @@
-local cmd = vim.cmd
-local g = vim.g
-local o = vim.o
-local bo = vim.bo
-local wo = vim.wo
-
--- General Settings, in LUA
-bo.expandtab = true
-bo.shiftwidth = 2
-bo.softtabstop = 2
+require('general')
+require('packages')
 
 -- Keybindings
 -- Set using vim.api.nvim_set_keymap({mode},{keymap},{mapped to}, {options})
-
-
--- Package Manager
--- Run yay -S nvim-packer-git
-require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
-end)
---
+++ neovim/.config/nvim/lua/packages.lua
@@ -0,0 +1,6 @@
+-- Package Manager
+-- Run yay -S nvim-packer-git
+require('packer').startup(function()
+        use 'wbthomason/packer.nvim'
+end)
+
