29f017f374e1d8b127efa4df40c7dea2c459ebcc
Antonio Aslan@ <aslan.suarez@gmail.com>
2022-05-13 11:15:25 +0200

    Neovim 05 Plugins

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -2,6 +2,7 @@ require('general')
 require('packages')
 require('gui')
 require('options')
+require('keymaps')
 
 -- Keybindings
 -- Set using vim.api.nvim_set_keymap({mode},{keymap},{mapped to}, {options})
--- neovim/.config/nvim/lua/options.lua
+++ neovim/.config/nvim/lua/options.lua
@@ -32,7 +32,7 @@ vim.opt.signcolumn = "yes"                      -- always show the sign column, 
 vim.opt.wrap = true                            -- display lines as one long line
 vim.opt.scrolloff = 8                           -- is one of my fav
 vim.opt.sidescrolloff = 8
-vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
+vim.opt.guifont = "Iosevska:h16"               -- the font used in graphical neovim applications
 
 vim.opt.shortmess:append "c"
 
