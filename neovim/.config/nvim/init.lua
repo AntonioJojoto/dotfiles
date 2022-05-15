a921c2eeeb3b1a7bc394726d113d7c15fff1d924
Antonio Aslan@ <aslan.suarez@gmail.com>
2022-05-15 16:13:38 +0200

    Frindship ended with St, now alacritty

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -3,6 +3,9 @@ require('packages')
 require('gui')
 require('options')
 require('keymaps')
+require('completion')
+
 
 -- Keybindings
 -- Set using vim.api.nvim_set_keymap({mode},{keymap},{mapped to}, {options})
+
--- neovim/.config/nvim/lua/packages.lua
+++ neovim/.config/nvim/lua/packages.lua
@@ -44,7 +44,7 @@ packer.init {
 -- Install your packages here
 require('packer').startup(function()
         use 'wbthomason/packer.nvim'
-        ------------------------------------------------------------------------
+  ------------------------------------------------------------------------
 	--------------------------------- GUI ----------------------------------
 	------------------------------------------------------------------------
 	-- Status line
@@ -62,6 +62,17 @@ require('packer').startup(function()
 	use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+  -- Completion
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
 
   -- Automatically set up your configuration after cloning packer.nvim
