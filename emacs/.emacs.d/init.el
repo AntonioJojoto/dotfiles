4a9d0f6209b82c1a98b687ebb540483947d25e9a
Antonio Aslan@ <aslan.suarez@gmail.com>
2024-03-14 20:24:10 +0100

    Added sessions functions

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -257,6 +257,7 @@
                         (projects . 5)
                         (agenda . 5)
                         (registers . 5)))
+
 ;; avy
 (avy-setup-default)
 
@@ -596,7 +597,7 @@ consult-org-roam-forward-links
   ;;"ar" '(org-ref-helm-insert-ref-link :which-key "org-ref insert link")
   ;;"af" '(org-fragtog-mode :which-key "Toggle fragtop mode")
   
-  ;; help in emacs
+  ;; 
 			      
   ;; ...
 )
@@ -632,5 +633,60 @@ consult-org-roam-forward-links
                  (buffer-modified-p))
         (save-buffer)))))
 
+;; Autosaving all .org
 (run-with-timer 0 120 'save-all-org-buffers)
 (put 'downcase-region 'disabled nil)
+
+(setq-default sessions-path "~/.emacs.d/sessions")
+(setq-default sessions-name nil)
+
+(defun sessions-dir ()
+(concat sessions-path "/" sessions-name))
+
+(defun sessions-list ()
+ (seq-remove (lambda (x) (or (string= x ".") (string= x ".."))) (directory-files sessions-path)))
+
+(defun sessions ()
+ (interactive)
+ (message "Available sessions: %s" (sessions-list)))
+
+
+(defun sessions-set-name (name)
+ (interactive
+  (list
+   (completing-read "Enter session name: " (sessions-list))))
+ (setq sessions-name name))
+
+
+(defun sessions-name ()
+ (interactive)
+ (message "name: %s" sessions-name))
+
+
+(defun sessions-save ()
+ (interactive)
+ (when (null sessions-name)
+  (call-interactively 'sessions-set-name))
+ (let ((dir (sessions-dir)))
+  (mkdir dir t)
+  (desktop-save dir))
+ (sessions-name))
+
+
+(defun sessions-close ()
+ (interactive)
+ (when (and sessions-name
+        (y-or-n-p (concat "Save current session '" sessions-name "'?")))
+  (call-interactively 'sessions-save)))
+
+
+(defun sessions-open ()
+ (interactive)
+ (when sessions-name
+  (call-interactively 'sessions-close))
+ (call-interactively 'sessions-set-name)
+ (desktop-change-dir (sessions-dir)))
+
+;; auto save on emacs exit (killing the window)
+(add-hook 'delete-frame-functions (lambda () (sessions-close)))
+(add-hook 'kill-emacs-hook (lambda () (sessions-close)))
