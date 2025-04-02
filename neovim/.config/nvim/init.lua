019bdcac8f3ec5040e80e0c1eff2da3a9f838ec3
tony <aslan.suarez@gmail.com>
2025-04-02 08:17:59 +0200

    my newovim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -20,6 +20,9 @@ require("lazy").setup({
     lazy = false,
     branch = "v2.5",
     import = "nvchad.plugins",
+    config = function ()
+     require("nvchad.tabufline").enabled = true
+    end
   },
 
   { import = "plugins" },
@@ -28,9 +31,48 @@ require("lazy").setup({
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
-
+vim.opt.relativenumber = true
 require "options"
 require "nvchad.autocmds"
+
+
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-1>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-2>", function() harpoon:list():next() end)
+
+-- basic telescope configuration
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+    { desc = "Open harpoon window" })
 
 vim.schedule(function()
   require "mappings"
--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"stevearc/conform.nvim",
-		-- event = 'BufWritePre', -- uncomment for format on save
+		event = "BufWritePre", -- uncomment for format on save
 		opts = require("configs.conform"),
 	},
 
@@ -13,31 +13,51 @@ return {
 		end,
 	},
 	{
-		" williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "typescript-language-server",
-        "pyright",
-      }
-    }
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"ts_ls",
+				"pyright",
+				"lua_ls",
+				"clangd",
+				"eslint",
+				"cssls",
+				"html",
+			},
+		},
 	},
-
-  {
-    "windwp/nvim-ts-autotag",
-    ft = {
-      "javascript",
-      "javascriptreact",
-    },
-    config = function ()
-      require("nvim-ts-autotag").setup()
-    end
-  },
+	{
+		"windwp/nvim-ts-autotag",
+		ft = {
+			"javascript",
+			"javascriptreact",
+		},
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
 
 	{
 		"nvim-treesitter/nvim-treesitter",
-			ensure_installed = {
-				"vim", "lua", "vimdoc",
-	     "html", "css", "c", "python", "cpp", "bash", "cpp", "javascript", "typescript"
-			},
+		ensure_installed = {
+			"vim",
+			"lua",
+			"vimdoc",
+			"html",
+			"css",
+			"c",
+			"python",
+			"cpp",
+			"bash",
+			"cpp",
+			"javascript",
+			"typescript",
 		},
+	},
+	"ThePrimeagen/vim-be-good",
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 }
